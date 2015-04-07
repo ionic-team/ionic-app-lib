26b50e6484a16c8426e7f99d563da2a6867fed65
jbavari <josh@drifty.com>
2015-04-06 23:12:41 -0600

    Adding in proper require statements, creating module.exports

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var start = require('./lib/start'),
+var serve = require('./lib/serve'),
+    start = require('./lib/start'),
     upload = require('./lib/upload'),
     events = require('./lib/events');
 
 module.exports = {
   events: events,
+  serve: serve,
   start: start,
   upload: upload
 }
