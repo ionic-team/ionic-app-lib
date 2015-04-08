0135704fe4251d331b1eabbb3a4dd0ba8b230e2f
jbavari <josh@drifty.com>
2015-04-08 10:37:21 -0600

    Refactored info to do exports properly and included basic tests

---
+++ spec/info.spec.js
@@ -0,0 +1,30 @@
+var Info = require('../lib/info'),
+    Q = require('q'),
+    events = require('../lib/events'),
+    helpers = require('./helpers');
+
+ddescribe('Info', function() {
+
+  it('should have info defined', function() {
+    expect(Info).toBeDefined();
+  });
+
+  it('should have gatherInfo defined', function() {
+    expect(Info.gatherInfo).toBeDefined();
+  });
+
+  it('should have gatherInfo call other helper methods', function() {
+    spyOn(Info, 'getIonicVersions');
+    spyOn(Info, 'getNodeVersion');
+    spyOn(Info, 'getOsEnvironment');
+    spyOn(Info, 'getCordovaInfo');
+
+    Info.gatherInfo();
+
+    expect(Info.getIonicVersions).toHaveBeenCalled();
+    expect(Info.getNodeVersion).toHaveBeenCalled()
+    expect(Info.getOsEnvironment).toHaveBeenCalled();
+    expect(Info.getCordovaInfo).toHaveBeenCalled();    
+  });
+
+});
