d47b289d0fdd677215fbcce05cbd85db6f4f0134
jbavari <josh@drifty.com>
2015-04-09 17:14:36 -0600

    Additional commands for existing platform. Adding method fr running cordova apps, and removing platforms

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-var events = require('./lib/events'),
+var cordova = require('./lib/cordova'),
+    events = require('./lib/events'),
     info = require('./lib/info'),
     serve = require('./lib/serve'),
     start = require('./lib/start'),
+    state = require('./lib/state'),
     upload = require('./lib/upload'),
     utils = require('./lib/utils');
 
 module.exports = {
+  cordova: cordova,
   events: events,
   info: info,
   serve: serve,
   start: start,
+  state: state,
   upload: upload,
   utils: utils
 }
