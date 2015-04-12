24f28e3b2e657c4e7dab9c0ac323821d6aa551e0
jbavari <josh@drifty.com>
2015-04-12 09:27:06 -0600

    Adding in the browser changes - including basic screen test

---
+++ spec/browser.spec.js
@@ -0,0 +1,11 @@
+var Browser = require('../lib/browser'),
+    events = require('../lib/events'),
+    helpers = require('./helpers');
+
+describe('Browser', function() {
+
+  it('should have Browser defined', function() {
+    expect(Browser).toBeDefined();
+  });
+
+});
