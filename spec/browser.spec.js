c5a4bb1042509ea12dc6a45f57aae3d1d25ab24e
jbavari <josh@drifty.com>
2015-04-14 23:32:52 -0600

    Adding tests for browser and cordova

--- spec/browser.spec.js
+++ spec/browser.spec.js
@@ -1,11 +1,100 @@
 var Browser = require('../lib/browser'),
     events = require('../lib/events'),
-    helpers = require('./helpers');
+    helpers = require('./helpers'),
+    shelljs = require('shelljs'),
+    Q = require('q');
+
+// spyOn(shelljs)
 
 describe('Browser', function() {
 
+  var testDirectory = '/some/fake/directory';
+
   it('should have Browser defined', function() {
     expect(Browser).toBeDefined();
+  });
+
+  describe('#addBrowser', function() {
+
+    it('should fail with no parameters passed', function() {
+      expect(function() {
+        Browser.addBrowser();
+      }).toThrow('You must pass a directory to run this command')
+    });
+
+    it('should fail with no browser passed', function() {
+      expect(function() {
+        Browser.addBrowser(testDirectory);
+      }).toThrow('You must pass a browser to be installed');
+    });
+
+    it('should call installCrosswalk when passed the crosswalk browser', function(done) {
+      spyOn(Browser, 'installCrosswalk');
+      Q()
+      .then(function() {
+        return Browser.addBrowser(testDirectory, 'crosswalk');
+      })
+      .then(function(){
+        expect(Browser.installCrosswalk).toHaveBeenCalledWith(testDirectory, Browser.defaultCrosswalkVersion);
+      })
+      .catch(function(ex){
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
+
+    it('should log to the user no accepted browser was passed', function(done) {
+      // spyOn(Browser, 'installDefaultBrowser');
+      spyOn(events, 'emit');
+      Q()
+      .then(function() {
+        return Browser.addBrowser(testDirectory, 'default');
+      })
+      .then(function(){
+        // expect(Browser.installDefaultBrowser).toHaveBeenCalledWith(testDirectory);
+        expect(events.emit).toHaveBeenCalledWith('log', 'No accepted browser was specified.'.red.bold);
+      })
+      .catch(function(ex){
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
+
+  });
+
+  describe('#installCrosswalk', function() {
+    it('should call the appropriate methods to install crosswalk', function(done) {
+      spyOn(Browser, 'downloadFiles').andReturn(Q());
+      var methods = [
+        'removeAndroidProject', 
+        'removeCrosswalkEngines',
+        'addCordova40xProject',
+        'addCrosswalkPlugin',
+        'addGradleProperties',
+        'addSplashScreenPlugin'
+      ];
+      
+      methods.forEach(function(method) {
+        spyOn(Browser, method);
+      });
+
+      Q()
+      .then(function() {
+        return Browser.installCrosswalk(testDirectory, Browser.defaultCrosswalkVersion);
+      })
+      .then(function(){
+        // expect(Browser.downloadFiles).toHaveBeenCalledWith(testDirectory, Browser.defaultCrosswalkVersion);
+        methods.forEach(function(method) {
+          expect(Browser[method]).toHaveBeenCalledWith(testDirectory);
+        })
+      })
+      .catch(function(ex){
+        console.log(ex);
+        console.log(ex.stack)
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
   });
 
 });
--- spec/cordova.spec.js
+++ spec/cordova.spec.js
@@ -1,12 +1,121 @@
 var Cordova = require('../lib/cordova'),
+    cordova = require('cordova-lib').cordova.raw,
     Q = require('q'),
     events = require('../lib/events'),
     helpers = require('./helpers');
+
+var testDirectory = '/test/directory',
+    testPluginId = 'org.apache.cordova.device';
 
 describe('Cordova', function() {
 
   it('should have Cordova defined', function() {
     expect(Cordova).toBeDefined();
   });
+
+  it('should call cordova.platform when addPlatform is called', function(done) {
+    spyOn(cordova, 'platform').andReturn(Q());
+    Q()    
+    .then(function() {
+      return Cordova.addPlatform(testDirectory, 'ios');
+    })
+    .then(function() {
+      expect(cordova.platform).toHaveBeenCalledWith('add', ['ios'], {});
+    })
+    .catch(function(data) {
+      console.log(data);
+      expect('this').toBe('not this');
+    })
+    .fin(done);
+  });
+
+  it('should call cordova.platform when removePlatform is called', function(done) {
+    spyOn(cordova, 'platform').andReturn(Q());
+    Q()    
+    .then(function() {
+      return Cordova.removePlatform(testDirectory, 'ios');
+    })
+    .then(function() {
+      expect(cordova.platform).toHaveBeenCalledWith('remove', ['ios'], {});
+    })
+    .catch(function(data) {
+      console.log(data);
+      expect('this').toBe('not this');
+    })
+    .fin(done);
+  });
+
+  it('should call cordova.plugin when addPlugin is called', function(done) {
+    spyOn(cordova, 'plugin').andReturn(Q());
+    Q()    
+    .then(function() {
+      return Cordova.addPlugin(testDirectory, testPluginId);
+    })
+    .then(function() {
+      expect(cordova.plugin).toHaveBeenCalledWith('add', testPluginId);
+    })
+    .catch(function(data) {
+      console.log(data);
+      expect('this').toBe('not this');
+    })
+    .fin(done);
+  });
+
+  it('should call cordova.plugin when removePlugin is called', function(done) {
+    spyOn(cordova, 'plugin').andReturn(Q());
+    Q()    
+    .then(function() {
+      return Cordova.removePlugin(testDirectory, testPluginId);
+    })
+    .then(function() {
+      expect(cordova.plugin).toHaveBeenCalledWith('remove', testPluginId);
+    })
+    .catch(function(data) {
+      console.log(data);
+      expect('this').toBe('not this');
+    })
+    .fin(done);
+  });
+
+  it('should call cordova.run when runPlatform is called', function(done) {
+    spyOn(cordova, 'run').andReturn(Q());
+    
+    var options = {
+      platforms: ['ios'],
+      options: [],
+      verbose: false,
+      silent: false,
+      browserify: false
+    };
+
+    Q()    
+    .then(function() {
+      return Cordova.runPlatform(testDirectory, 'ios');
+    })
+    .then(function() {
+      expect(cordova.run).toHaveBeenCalledWith(options);
+    })
+    .catch(function(data) {
+      console.log(data);
+      expect('this').toBe('not this');
+    })
+    .fin(done);
+  });
+
+  // it('should call serve when livereload is passed', function(done) {
+
+  //   Q()    
+  //   .then(function() {
+  //     return Cordova.runPlatform(testDirectory, 'ios');
+  //   })
+  //   .then(function() {
+  //     expect(cordova.run).toHaveBeenCalledWith(options);
+  //   })
+  //   .catch(function(data) {
+  //     console.log(data);
+  //     expect('this').toBe('not this');
+  //   })
+  //   .fin(done);
+  // });  
 
 });
