e94d82fd1df3e7010a43b5006a7dca7dfd8d2a35
jbavari <josh@drifty.com>
2015-04-20 20:44:51 -0600

    Adding tests for cordova for having state saving platforms and removing platform

--- spec/cordova.spec.js
+++ spec/cordova.spec.js
@@ -2,120 +2,202 @@ var Cordova = require('../lib/cordova'),
     cordova = require('cordova-lib').cordova.raw,
     Q = require('q'),
     events = require('../lib/events'),
-    helpers = require('./helpers');
+    helpers = require('./helpers'),
+    state = require('../lib/state');
 
 var testDirectory = '/test/directory',
     testPluginId = 'org.apache.cordova.device';
 
-describe('Cordova', function() {
+ddescribe('Cordova', function() {
 
   it('should have Cordova defined', function() {
     expect(Cordova).toBeDefined();
   });
 
-  it('should call cordova.platform when addPlatform is called', function(done) {
-    spyOn(cordova, 'platform').andReturn(Q());
-    Q()    
-    .then(function() {
-      return Cordova.addPlatform(testDirectory, 'ios');
-    })
-    .then(function() {
-      expect(cordova.platform).toHaveBeenCalledWith('add', ['ios'], {});
-    })
-    .catch(function(data) {
-      console.log(data);
-      expect('this').toBe('not this');
-    })
-    .fin(done);
+  describe('#removePlatform', function() {
+    it('should call cordova.platform when removePlatform is called', function(done) {
+      spyOn(cordova, 'platform').andReturn(Q());
+      Q()    
+      .then(function() {
+        return Cordova.removePlatform(testDirectory, 'ios');
+      })
+      .then(function() {
+        expect(cordova.platform).toHaveBeenCalledWith('remove', ['ios'], {});
+      })
+      .catch(function(error) {
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
+
+    it('should call state.savePlatform when removePlatform is called to save platform', function(done) {
+      spyOn(cordova, 'platform').andReturn(Q());
+      spyOn(state, 'removePlatform').andReturn(Q());
+      Q()    
+      .then(function() {
+        return Cordova.removePlatform(testDirectory, 'ios', true);
+      })
+      .then(function() {
+        expect(state.removePlatform).toHaveBeenCalledWith(testDirectory, 'ios');
+      })
+      .catch(function(error) {
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
+
+    it('should not call state.savePlatform when removePlatform is called to save platform', function(done) {
+      spyOn(cordova, 'platform').andReturn(Q());
+      spyOn(state, 'removePlatform').andReturn(Q());
+      Q()    
+      .then(function() {
+        return Cordova.removePlatform(testDirectory, 'ios');
+      })
+      .then(function() {
+        expect(state.removePlatform).not.toHaveBeenCalled();
+      })
+      .catch(function(error) {
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
   });
 
-  it('should call cordova.platform when removePlatform is called', function(done) {
-    spyOn(cordova, 'platform').andReturn(Q());
-    Q()    
-    .then(function() {
-      return Cordova.removePlatform(testDirectory, 'ios');
-    })
-    .then(function() {
-      expect(cordova.platform).toHaveBeenCalledWith('remove', ['ios'], {});
-    })
-    .catch(function(data) {
-      console.log(data);
-      expect('this').toBe('not this');
-    })
-    .fin(done);
+  describe('#addPlugin', function() {
+    it('should call cordova.plugin when addPlugin is called', function(done) {
+      spyOn(cordova, 'plugin').andReturn(Q());
+      Q()    
+      .then(function() {
+        return Cordova.addPlugin(testDirectory, testPluginId);
+      })
+      .then(function() {
+        expect(cordova.plugin).toHaveBeenCalledWith('add', testPluginId);
+      })
+      .catch(function(data) {
+        console.log(data);
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
   });
 
-  it('should call cordova.plugin when addPlugin is called', function(done) {
-    spyOn(cordova, 'plugin').andReturn(Q());
-    Q()    
-    .then(function() {
-      return Cordova.addPlugin(testDirectory, testPluginId);
-    })
-    .then(function() {
-      expect(cordova.plugin).toHaveBeenCalledWith('add', testPluginId);
-    })
-    .catch(function(data) {
-      console.log(data);
-      expect('this').toBe('not this');
-    })
-    .fin(done);
+  describe('#removePlugin', function() {
+    it('should call cordova.plugin when removePlugin is called', function(done) {
+      spyOn(cordova, 'plugin').andReturn(Q());
+      Q()    
+      .then(function() {
+        return Cordova.removePlugin(testDirectory, testPluginId);
+      })
+      .then(function() {
+        expect(cordova.plugin).toHaveBeenCalledWith('remove', testPluginId);
+      })
+      .catch(function(data) {
+        console.log(data);
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
   });
 
-  it('should call cordova.plugin when removePlugin is called', function(done) {
-    spyOn(cordova, 'plugin').andReturn(Q());
-    Q()    
-    .then(function() {
-      return Cordova.removePlugin(testDirectory, testPluginId);
-    })
-    .then(function() {
-      expect(cordova.plugin).toHaveBeenCalledWith('remove', testPluginId);
-    })
-    .catch(function(data) {
-      console.log(data);
-      expect('this').toBe('not this');
-    })
-    .fin(done);
+  describe('#runPlatform', function() {
+    it('should call cordova.run when runPlatform is called', function(done) {
+      spyOn(cordova, 'run').andReturn(Q());
+      
+      var options = {
+        platforms: ['ios'],
+        options: [],
+        verbose: false,
+        silent: false,
+        browserify: false
+      };
+
+      Q()    
+      .then(function() {
+        return Cordova.runPlatform(testDirectory, 'ios');
+      })
+      .then(function() {
+        expect(cordova.run).toHaveBeenCalledWith(options);
+      })
+      .catch(function(data) {
+        console.log(data);
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
+
+    // it('should call serve when livereload is passed', function(done) {
+
+    //   Q()    
+    //   .then(function() {
+    //     return Cordova.runPlatform(testDirectory, 'ios');
+    //   })
+    //   .then(function() {
+    //     expect(cordova.run).toHaveBeenCalledWith(options);
+    //   })
+    //   .catch(function(data) {
+    //     console.log(data);
+    //     expect('this').toBe('not this');
+    //   })
+    //   .fin(done);
+    // }); 
   });
 
-  it('should call cordova.run when runPlatform is called', function(done) {
-    spyOn(cordova, 'run').andReturn(Q());
-    
-    var options = {
-      platforms: ['ios'],
-      options: [],
-      verbose: false,
-      silent: false,
-      browserify: false
-    };
+  describe('#addPlatform', function() {
+    it('should set environment variable for cordova pwd', function(done) {
+      spyOn(cordova, 'platform').andReturn(Q());
+      Q()
+      .then(function() {
 
-    Q()    
-    .then(function() {
-      return Cordova.runPlatform(testDirectory, 'ios');
-    })
-    .then(function() {
-      expect(cordova.run).toHaveBeenCalledWith(options);
-    })
-    .catch(function(data) {
-      console.log(data);
-      expect('this').toBe('not this');
-    })
-    .fin(done);
-  });
+      })
+      .fin(done);
+    });
 
-  // it('should call serve when livereload is passed', function(done) {
+    it('should call cordova.platform when addPlatform is called', function(done) {
+      spyOn(cordova, 'platform').andReturn(Q());
+      Q()    
+      .then(function() {
+        return Cordova.addPlatform(testDirectory, 'ios');
+      })
+      .then(function() {
+        expect(cordova.platform).toHaveBeenCalledWith('add', ['ios'], {});
+      })
+      .catch(function(data) {
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
 
-  //   Q()    
-  //   .then(function() {
-  //     return Cordova.runPlatform(testDirectory, 'ios');
-  //   })
-  //   .then(function() {
-  //     expect(cordova.run).toHaveBeenCalledWith(options);
-  //   })
-  //   .catch(function(data) {
-  //     console.log(data);
-  //     expect('this').toBe('not this');
-  //   })
-  //   .fin(done);
-  // });  
+    it('should call state.savePlatform when addPlatform is called with save platform', function(done) {
+      spyOn(cordova, 'platform').andReturn(Q());
+      spyOn(state, 'savePlatform').andReturn(Q());
+      Q()    
+      .then(function() {
+        return Cordova.addPlatform(testDirectory, 'ios', true);
+      })
+      .then(function() {
+        expect(state.savePlatform).toHaveBeenCalledWith(testDirectory, 'ios');
+      })
+      .catch(function(data) {
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
+
+    it('should not call state.savePlatform when addPlatform is called without save platform', function(done) {
+      spyOn(cordova, 'platform').andReturn(Q());
+      spyOn(state, 'savePlatform').andReturn(Q());
+      Q()    
+      .then(function() {
+        return Cordova.addPlatform(testDirectory, 'ios');
+      })
+      .then(function() {
+        expect(state.savePlatform).not.toHaveBeenCalled();
+      })
+      .catch(function(data) {
+        expect('this').toBe('not this');
+      })
+      .fin(done);
+    });
+  }); 
 
 });
