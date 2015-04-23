e5637812a41b570c75d249efa2026e0f2e591184
jbavari <josh@drifty.com>
2015-04-23 09:49:32 -0600

    Adding in opbeat and including tests for those

---
+++ spec/opbeat.spec.js
@@ -0,0 +1,42 @@
+var Opbeat = require('../lib/opbeat'),
+    events = require('../lib/events'),
+    helpers = require('./helpers');
+
+ddescribe('Opbeat', function() {
+  it('should have state defined', function() {
+    expect(Opbeat).toBeDefined();
+  });
+
+  it('should init the opbeat client with the correct settings', function() {
+    spyOn(Opbeat, 'createOpbeatClient');
+    Opbeat.createCliClient();
+    expect(Opbeat.createOpbeatClient).toHaveBeenCalledWith(Opbeat.cliClientOptions);
+  });
+
+  it('should use the options you pass to init the client', function() {
+    spyOn(Opbeat, 'createOpbeatClient');
+    var options = {
+      organizationId: 'orgId',
+      appId: 'appId',
+      secretToken: 'secId',
+      clientLogLevel: 'cliId',
+      prerequestMethod: function() {}
+    };
+    Opbeat.createCliClient(options);
+    expect(Opbeat.createOpbeatClient).toHaveBeenCalledWith(options);
+  });
+
+
+  it('should use the options you pass to init the gui client', function() {
+    spyOn(Opbeat, 'createOpbeatClient');
+    var options = {
+      organizationId: 'orgId',
+      appId: 'appId',
+      secretToken: 'secId',
+      clientLogLevel: 'cliId',
+      prerequestMethod: function() {}
+    };
+    Opbeat.createGuiClient(options);
+    expect(Opbeat.createOpbeatClient).toHaveBeenCalledWith(options);
+  });
+});
