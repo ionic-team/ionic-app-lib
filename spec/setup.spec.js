d02edd0e3a5990a9feb514f298d74e1277dc7d10
jbavari <josh@drifty.com>
2015-05-22 15:40:41 -0500

    Feature(setup): Extract setup from cli to library, add setup facebook command

--- spec/setup.spec.js
+++ spec/setup.spec.js
@@ -1,4 +1,6 @@
-var setup = require('../lib/setup');
+var Q = require('q'),
+    setup = require('../lib/setup'),
+    testDir = '/some/ionic/app';
 
 describe('Setup', function() {
 
@@ -6,8 +8,24 @@ describe('Setup', function() {
     expect(setup).toBeDefined();
   });
 
-  it('should set up sass correctly', function() {
-    setup.sassSetup();
+  it('should set up sass correctly', function(done) {
+    spyOn(setup, 'npmInstall').andReturn(Q());
+    spyOn(setup, 'modifyIndexFile').andReturn(Q());
+    spyOn(setup, 'buildSass').andReturn(Q());
+    
+    Q()
+    .then(function(){
+      setup.sassSetup(testDir);
+    })
+    .then(function(){
+      expect(setup.npmInstall).toHaveBeenCalledWith(testDir);
+      expect(setup.modifyIndexFile).toHaveBeenCalledWith(testDir);
+      expect(setup.buildSass).toHaveBeenCalledWith(testDir);
+    })
+    .catch(function(ex){
+      expect('this').toBe('not this');
+    })
+    .fin(done);
   });
 
   xit('should install npm modules correctly', function(){
