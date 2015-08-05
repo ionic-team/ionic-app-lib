0ba15f25d9b787ab9aad7ea7f875f52a19cd9e8f
jbavari <josh@drifty.com>
2015-08-05 17:44:04 -0600

    Fix(index): Add spec to ensure current dependencies have the attribtues available to be exported. Added semver due to requirement from consumers.

---
+++ spec/index.spec.js
@@ -0,0 +1,27 @@
+var index = require('../index'),
+    logging = require('../lib/logging');
+
+ddescribe('index', function() {
+
+  it('should have index defined', function() {
+    expect(index).toBeDefined();
+  });
+
+  function testForProperty(input) {
+    it('should have ' + input + ' available', function() {
+      expect(index[input]).toBeDefined();
+    });
+  }
+
+  var objs = ['browser', 'configXml', 'cordova', 'events', 'hooks', 'info', 
+              'ioConfig', 'login', 'logging', 'multibar', 'opbeat', 'project', 
+              'share', 'semver', 'serve', 'settings', 'setup', 'start', 'state', 
+              'upload', 'utils'];
+  // Doing it this way to give better failure messages. 
+  // Ensures all commands are available currently from
+  objs.forEach(function(obj) {
+    // expect(index[obj], obj).toBeDefined();
+    testForProperty(obj);
+  });
+
+});
