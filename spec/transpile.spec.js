f5bbc3fbb8a64b9fd721a6ac2e2d5b26f031ffe1
jbavari <josh@drifty.com>
2015-10-20 15:02:50 -0600

    Fix(transpile): Remove default transpile webpack.config. Create new webpack config based on app directory passed. That way we avoid pathing issues between mac and windows

--- spec/transpile.spec.js
+++ spec/transpile.spec.js
@@ -1,9 +1,21 @@
 var transpile = require('../lib/transpile');
 
-describe('index', function() {
+ddescribe('transpile', function() {
 
-  it('should have index defined', function() {
+  it('should have transpile defined', function() {
     expect(transpile).toBeDefined();
+  });
+
+  it('should get webpack configuration based on app path passed', function() {
+    var fakePath = '/ionic/app/path';
+    var webpackConfig = transpile.webpackConfigForPath(fakePath);
+    expect(webpackConfig).toBeDefined();
+    expect(webpackConfig.entry).toBeDefined();
+    expect(webpackConfig.output).toBeDefined();
+    expect(webpackConfig.output.path).toBe('/ionic/app/path/www/app/build/js');
+    expect(webpackConfig.output.filename).toBe('app.bundle.js');
+    expect(webpackConfig.module.loaders[0].include).toBe('/ionic/app/path/www/app');
+    expect(webpackConfig.module.loaders[1].include).toBe('/ionic/app/path/www/app');
   });
 
 });
