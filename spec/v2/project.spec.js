8461895fe96ec81b633f8c066eec15aa82a677d5
jbavari <josh@drifty.com>
2015-10-27 08:16:49 -0600

    Feature(project): Add v2 version of project file. Add basic specs for create, load, save

---
+++ spec/v2/project.spec.js
@@ -0,0 +1,164 @@
+var fs = require('fs'),
+    helpers = require('../helpers'),
+    path = require('path'),
+    Project = require('../../lib/v2/project'),
+    shell = require('shelljs'),
+    logging = require('../../lib/logging');
+
+logging.logger = helpers.testingLogger;
+
+var testDir = '/ionic/v2/project';
+
+var tmpDir = helpers.tmpDir('create_test');
+var appName = 'TestIonic';
+var projectDir = path.join(tmpDir, appName);
+
+
+var spyOnFileSystem = function(data) {
+  data = data ? JSON.stringify(data) : JSON.stringify(Project.PROJECT_DEFAULT);
+  spyOn(fs, 'existsSync').andReturn(true);
+  spyOn(fs, 'readFileSync').andReturn(data);
+};
+
+ddescribe('Project', function() {
+  var data = Project.PROJECT_DEFAULT;
+
+  // beforeEach(function() {
+  //   spyOnFileSystem(data);
+  // });  
+
+  it('should have Project defined', function() {
+    expect(Project).toBeDefined();
+  });
+
+  xdescribe('#create', function() {
+    it('should create a default project file', function() {
+      // spyOn(Project, 'set').andCallThrough();
+      // spyOn(Project, 'wrap').andCallThrough();
+      spyOn(fs, 'statSync');
+      var project = Project.create(testDir, 'test');
+      expect(project.get('name')).toBe('test');
+      expect(project.get('defaultBrowser'), 'chrome');
+      expect(project.get('defaultBrowser'), 'chrome');
+      expect(project.get('proxies'), null);
+      expect(project.get('sassEntryPath'), '/www/app/scss');
+      expect(project.get('sassOutputPath'), '/www/build/css');
+      expect(project.get('sassWatchPattern'), ['www/**/*.scss']);
+      expect(project.get('htmlWatchPattern'), ['www/**/*.html']);
+    });
+  });
+
+  xdescribe('#load', function() {
+    var project = Project.load(testDir);
+    expect(project).toBeDefined();
+  });
+
+  describe('#save', function() {
+    it('should save the configuration', function() {
+      spyOn(fs, 'mkdirSync'); //mock out the creation of mkdirSync for project folder
+      spyOn(fs, 'writeFileSync'); // mock out writing file
+      Project.save(testDir, Project.PROJECT_DEFAULT);
+      
+      var projectFileDir = path.join(testDir, Project.PROJECT_FILE_DIR);
+      var projectFilePath = path.join(testDir, Project.PROJECT_FILE_DIR, Project.PROJECT_FILE);
+      var projectContents = 'module.exports = ' + JSON.stringify(Project.PROJECT_DEFAULT);
+      
+      expect(fs.mkdirSync).toHaveBeenCalledWith(projectFileDir);
+      expect(fs.writeFileSync).toHaveBeenCalledWith(projectFilePath, projectContents)
+    });
+  });
+
+  describe('#load', function() {
+    var projectFile;
+
+    beforeEach(function() {
+      //Create the test project file
+      shell.rm('-rf', projectDir);
+      shell.mkdir('-p', tmpDir);
+      shell.mkdir('-p', projectDir);
+      projectFile = Project.create(projectDir, 'test');
+      projectFile.save();
+    });
+
+    it('should load a saved file', function() {
+      var project = Project.load(projectDir);
+      expect(projectFile.get('name')).toBe(project.get('name'));
+      expect(projectFile.get('sassEntryPath')).toBe(project.get('sassEntryPath'));
+    });
+  })
+
+  // describe('#wrap', function() {
+  //   var project = null;
+
+  //   beforeEach(function() {
+  //     project = Project.wrap(testDir, data);
+  //   });
+
+  //   it('should wrap the methods correctly', function() {
+  //     expect(project).toBeDefined();
+  //     expect(project.get).toBeDefined();
+  //     expect(project.remove).toBeDefined();
+  //     expect(project.set).toBeDefined();
+  //     expect(project.save).toBeDefined();
+  //   });
+
+  //   it('should call Project.get from wrapped object', function() {
+  //     spyOn(Project, 'get').andCallThrough();
+  //     var appId = project.get('app_id');
+
+  //     expect(appId).toBe(data.app_id);
+  //     expect(Project.get).toHaveBeenCalledWith(data, 'app_id');
+  //   });
+
+  //   it('should call Project.set from wrapped object', function() {
+  //     spyOn(Project, 'set').andCallThrough();
+  //     project.set('app_id', 'uid');
+
+  //     expect(Project.set).toHaveBeenCalledWith(data, 'app_id', 'uid');
+
+  //     var appId = project.get('app_id');
+
+  //     expect(appId).toBe('uid');
+  //   });
+
+  //   it('should call Project.remove from wrapped object', function() {
+  //     spyOn(Project, 'remove').andCallThrough();
+  //     project.set('app_id', 'uid');
+  //     expect(project.get('app_id')).toBe('uid');
+  //     project.remove('app_id');
+  //     expect(Project.remove).toHaveBeenCalledWith(data, 'app_id');
+  //     expect(project.get('app_id')).toBe('');
+  //   });
+
+  //   it('should call Project.save from wrapped object', function() {
+  //     spyOn(Project, 'save');
+  //     project.set('app_id', 'uid');
+
+  //     project.save();
+
+  //     expect(Project.save).toHaveBeenCalledWith(testDir, data);
+  //   });
+
+  // });
+
+  // describe('#load', function() {
+
+  //   it('should default to cwd if no app directory passed', function() {
+  //     var project = Project.load();
+  //     expect(project).toBeDefined();
+  //   });
+
+  //   it('should wrap the project object with app directory and data', function() {
+  //     spyOn(Project, 'wrap').andCallThrough();
+  //     var project = Project.load(testDir);
+  //     expect(Project.wrap).toHaveBeenCalledWith(testDir, data);
+  //   });
+
+  //   it('should load a base ionic.project file that exists', function() {
+  //     var project = Project.load(testDir);
+  //     expect(fs.existsSync).toHaveBeenCalledWith(path.join(testDir, Project.PROJECT_FILE));
+  //     expect(fs.readFileSync).toHaveBeenCalledWith(path.join(testDir, Project.PROJECT_FILE));
+  //   });
+  // });
+
+});
