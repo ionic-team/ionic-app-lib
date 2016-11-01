ff35819b3d27ab00d81fc559be10f1e45307b52a
Josh Thomas <jthoms1@gmail.com>
2016-11-01 11:10:37 -0500

    Info speed up (#130)
    * feat(): rewrite info to be async.
    
    * fix(): rewrite get info to make it async.

--- spec/info.spec.js
+++ spec/info.spec.js
@@ -2,26 +2,9 @@ var Info = require('../lib/info');
 
 describe('Info', function() {
 
-  it('should have info defined', function() {
-    expect(Info).toBeDefined();
-  });
-
-  it('should have gatherInfo defined', function() {
-    expect(Info.gatherInfo).toBeDefined();
-  });
-
   it('should have gatherInfo call other helper methods', function() {
-    spyOn(Info, 'getIonicLibVersion');
-    spyOn(Info, 'getNodeVersion');
-    spyOn(Info, 'getOsEnvironment');
-    spyOn(Info, 'getCordovaInfo');
+    var info = Info.gatherInfo();
 
-    Info.gatherInfo();
-
-    expect(Info.getIonicLibVersion).toHaveBeenCalled();
-    expect(Info.getNodeVersion).toHaveBeenCalled();
-    expect(Info.getOsEnvironment).toHaveBeenCalled();
-    expect(Info.getCordovaInfo).toHaveBeenCalled();
+    console.log(info);
   });
-
 });
